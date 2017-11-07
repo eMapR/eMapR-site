0a1ea1a5db96f776756243bb5d850b6fd65f81ee
Justin Braaten <jstnbraaten@gmail.com>
2017-11-06 16:12:49 -0900

    changes

--- resources/js/people_controller_get.js
+++ resources/js/people_controller_get.js
@@ -1,8 +1,8 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('peopleCtrl', ['$scope', '$http', function ($scope, $http, $sce) {
+myApp.controller('peopleCtrl', ['$scope', '$http', function ($scope, $http) {
   //$http.get('http://ltweb.ceoas.oregonstate.edu/lab_site_dev/resources/data/people.json')
-  $scope.$sce = $sce;
+
   $http.get('resources/data/people.json')
   .success(function (data, status, headers, config) {
 	$scope.people = data;
